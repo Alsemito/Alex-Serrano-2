5357d05ed93ca98f9949cb73f4a521910b4f2fcc
Alex Serrano Marull <alex.serrano01@estudiant.upf.edu>
2023-04-18 20:29:00 +0200

    last twicks

--- Client.py
+++ Client.py
@@ -20,6 +20,7 @@ def operation_name(operator_: str) -> str:
 welcome = requests.get("http://localhost:5000/")
 print(welcome.text)
 
+
 while True:
 
     # With this, one is able to toggle between parts of the assignment
@@ -27,59 +28,61 @@ while True:
                    "[1] Manual input\n"
                    "[2] Input from file\n")
 
-    if choice == "1":
-        # Ask for the first value of the operation
-        a = input("Enter the first value of the operation ('q' to terminate): ")
-        if a == "q":
+    match choice:
+        case "1":
+            # Ask for the first value of the operation
+            a = input("Enter the first value of the operation ('q' to terminate): ")
+            if a == "q":
+                break
+
+            aux = 0
+            while aux == 0:
+                # Ask for the operation that the user wants to perform
+                operator = input("Enter the operation you want to perform (+, -, *, /): ")
+                if operator != "+" and operator != "-" and operator != "*" and operator != "/":
+                    print("Invalid operation")
+                else:
+                    # Substitute the operator for the word to access the right url (in the server side)
+                    operation = operation_name(operator)
+                    aux = 1
+
+            # Ask for the second value
+            b = input("Enter the second value of the operation ('q' to terminate): ")
+            if b == "q":
+                break
+
+            # Create and request the desired operation
+            url = f"http://localhost:5000/{operation}?a={a}&b={b}"
+            response = requests.get(url)
+
+            print(f"\nThe answer to {a} {operator} {b} is: ", response.text + "\n")
+
+        case "2":
+            log = input("\nDo you want a log of all the operations and results? [y][n]\n")
+            # Open the sample file
+            with open("sample.csv", "r") as f:
+                reader = csv.reader(f)
+                # Iterate the entire file and print every operation
+                for line in reader:
+                    a = str(line[0])
+                    operator = str(line[1])
+                    operation = operation_name(operator)
+                    b = str(line[2])
+
+                    # Create and request the desired operation
+                    url = f"http://localhost:5000/{operation}?a={a}&b={b}"
+                    response = requests.get(url)
+
+                    print(f"The answer to {a} {operator} {b} is: ", response.text)
+
+                    # If the user wants, it can generate a log file as a .txt
+                    if log == "y":
+                        with open("log_file.txt", "a") as log_file:
+                            log_file.write(a + operator + b + " = " + response.text + "\n")
+
+        case "q":
             break
 
-        aux = 0
-        while aux == 0:
-            # Ask for the operation that the user wants to perform
-            operator = input("Enter the operation you want to perform (+, -, *, /): ")
-            if operator != "+" and operator != "-" and operator != "*" and operator != "/":
-                print("Invalid operation")
-            else:
-                # Substitute the operator for the word to access the right url (in the server side)
-                operation = operation_name(operator)
-                aux = 1
-
-        # Ask for the second value
-        b = input("Enter the second value of the operation ('q' to terminate): ")
-        if b == "q":
-            break
-
-        # Create and request the desired operation
-        url = f"http://localhost:5000/{operation}?a={a}&b={b}"
-        response = requests.get(url)
-
-        print(f"\nThe answer to {a} {operator} {b} is: ", response.text + "\n")
-
-    elif choice == "2":
-        log = input("\nDo you want a log of all the operations and results? [y][n]\n")
-        # Open the sample file
-        with open("sample.csv", "r") as f:
-            reader = csv.reader(f)
-            # Iterate the entire file and print every operation
-            for line in reader:
-                a = str(line[0])
-                operator = str(line[1])
-                operation = operation_name(operator)
-                b = str(line[2])
-
-                # Create and request the desired operation
-                url = f"http://localhost:5000/{operation}?a={a}&b={b}"
-                response = requests.get(url)
-
-                print(f"The answer to {a} {operator} {b} is: ", response.text)
-
-                # If the user wants, it can generate a log file as a .txt
-                if log == "y":
-                    with open('log_file.txt', 'a') as log_file:
-                        log_file.write(a + operator + b + " = " + response.text + "\n")
-
-    elif choice == "q":
-        break
-    else:
-        print("Enter a valid option")
+        case default:
+            print("Enter a valid option")
 
